2b2d94d6e1e0a5f15c7ef285744f6529ca8443b8
glopinous <glopinous@192.168.1.23>
2014-10-08 12:45:13 +0200

    Création d'un EJB pour gérer les "timer"

---
+++ src/main/java/fr/feedreader/buisness/TimerBuisness.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.feedreader.buisness;
+
+import fr.feedreader.models.Feed;
+import fr.feedreader.models.FeedItem;
+import fr.feedreader.websocket.UpdateFeed;
+import java.util.List;
+import java.util.Map;
+import javax.ejb.Schedule;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author philippe
+ */
+@Stateless
+public class TimerBuisness {
+    
+    @Inject
+    protected FeedBuisness feedBuisness;
+    
+    @Schedule(hour = "*", minute = "*/15", persistent = false)
+    public void updateFeed() {
+        Map<Feed, List<FeedItem>> newFeedItem = feedBuisness.parallelUpdateAllFeed();
+        UpdateFeed.notifyUpdateFeed(newFeedItem, feedBuisness.countUnread());
+    }
+
+}
