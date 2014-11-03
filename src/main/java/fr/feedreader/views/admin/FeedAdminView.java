c0eecc5dfb2a800475eb3be46f02a0486f6eb695
FICHET Philippe <philippe@easyflirt.com>
2014-11-03 12:58:24 +0100

    Gestion des actions (ajout, mise à jour, suppression et récupération d'article) des flux

--- src/main/java/fr/feedreader/views/admin/FeedAdminView.java
+++ src/main/java/fr/feedreader/views/admin/FeedAdminView.java
@@ -8,16 +8,18 @@ package fr.feedreader.views.admin;
 import com.vaadin.cdi.CDIView;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.server.FontAwesome;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.Table;
-import com.vaadin.ui.TextArea;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 import fr.feedreader.buisness.FeedBuisness;
 import fr.feedreader.models.Feed;
+import fr.feedreader.models.FeedItem;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -32,6 +34,8 @@ public class FeedAdminView extends VerticalLayout implements View {
     private boolean init = false;
     
     private long test = 0L;
+    
+    private Table feeds = new Table();
     
     @PostConstruct
     private void init() {
@@ -48,6 +52,8 @@ public class FeedAdminView extends VerticalLayout implements View {
             
         } else {
             removeAllComponents();
+            feeds.removeAllItems();
+            
             HorizontalLayout addLayout = new HorizontalLayout();
             addLayout.setWidth("100%");
             addLayout.setHeight("4em");
@@ -80,31 +86,107 @@ public class FeedAdminView extends VerticalLayout implements View {
             
             addComponent(addLayout);
             
-            Table feeds = new Table();
             feeds.setWidth("100%");
             feeds.setHeightUndefined();
             feeds.setPageLength(0);
             feeds.addContainerProperty("Url", TextField.class, "");
             feeds.addContainerProperty("Nom", TextField.class, "");
             feeds.addContainerProperty("Description", TextField.class, "");
+            feeds.addContainerProperty("Action", HorizontalLayout.class, "");
             
             List<Feed> findAll = feedBuisness.findAll();
             findAll.stream().forEach((feed) -> {
                 System.out.println("feed = " + feed);
-                TextField url = new TextField();
-                url.setValue(feed.getUrl());
-                url.setWidth("100%");
-                TextField name = new TextField();
-                name.setValue(feed.getName());
-                name.setWidth("100%");
-                TextField description = new TextField();
-                description.setValue(feed.getDescription());
-                description.setWidth("100%");
-                feeds.addItem(new Object[] { url, name, description }, feed.getId());
+                feeds.addItem(feedToTableRow(feed), feed.getId());
             });
             
             addComponent(feeds);
+            
+            addSumbit.addClickListener((eventClick) -> {
+                Feed feed = new Feed();
+                feed.setUrl(textFieldUrl.getValue());
+                feed.setName(textFieldName.getValue());
+                feed.setDescription(textFieldDescription.getValue());
+                try {
+                    feed = feedBuisness.add(feed);
+                    feeds.addItem(feedToTableRow(feed), feed.getId());
+                    showSuccessNotification("Ajout du flux réussi");
+                } catch(Exception e) {
+                    showErrorNotification("Ajout du flux échec", e.getMessage());
+                }
+            });
+
         }
+        
+    }
+    
+    public Object[] feedToTableRow(Feed feed) {
+        TextField url = new TextField();
+        url.setValue(feed.getUrl());
+        url.setWidth("100%");
+
+        TextField name = new TextField();
+        name.setValue(feed.getName());
+        name.setWidth("100%");
+
+        TextField description = new TextField();
+        description.setValue(feed.getDescription());
+        description.setWidth("100%");
+
+        Button update = new Button(FontAwesome.ROTATE_RIGHT);
+        Button save = new Button(FontAwesome.SAVE);
+        Button delete = new Button(FontAwesome.ERASER);
+        HorizontalLayout action = new HorizontalLayout(update, save, delete);
+        action.setWidth("100%");
+        action.setComponentAlignment(update, Alignment.MIDDLE_RIGHT);
+        action.setComponentAlignment(save, Alignment.MIDDLE_CENTER);
+        action.setComponentAlignment(delete, Alignment.MIDDLE_LEFT);
+        
+        update.addClickListener((eventClick) -> {
+            try {
+                List<FeedItem> refreshFeedItems = feedBuisness.refreshFeedItems(feed.getId());
+                showSuccessNotification("Récupération des nouveaux article réussi pour le flux \"" + feed.getName() + "\"");
+            } catch(Exception e) {
+                showErrorNotification("Récupération des nouveaux article en échec pour le flux \"" + feed.getName() + "\"", e.getLocalizedMessage());
+            }
+        });
+        
+        save.addClickListener((eventClick) -> {
+            try {
+                feed.setUrl(url.getValue());
+                feed.setDescription(description.getValue());
+                feed.setName(name.getValue());
+                Feed feedUpdated = feedBuisness.update(feed);
+                showSuccessNotification("Mise à jour du flux \"" + feedUpdated.getName() + "\" réussi");
+            } catch(Exception e) {
+                showErrorNotification("Mise à jour du flux \"" + feed.getName() + "\" échec", e.getMessage());
+            }
+        });
+        
+        delete.addClickListener((eventClick) -> {
+            try {
+                feedBuisness.delete(feed.getId());
+                feeds.removeItem(feed.getId());
+                showSuccessNotification("Suppression du flux \"" + feed.getName() + "\" réussi");
+            } catch(Exception e) {
+                showErrorNotification("Suppression du flux \"" + feed.getName() + "\" échec", e.getMessage());
+            }
+        });
+        
+        return new Object[] { url, name, description, action };
+    }
+    
+    public void showSuccessNotification(String title) {
+        Notification notification = new Notification(title, Notification.Type.HUMANIZED_MESSAGE);
+        notification.setDelayMsec(3000);
+        notification.setStyleName(ValoTheme.NOTIFICATION_SUCCESS);
+        notification.show(getUI().getPage());
+    }
+    
+    public void showErrorNotification(String title, String detail) {
+        Notification notification = new Notification(title, detail, Notification.Type.ERROR_MESSAGE);
+        notification.setDelayMsec(3000);
+        notification.show(getUI().getPage());
     }
     
 }
