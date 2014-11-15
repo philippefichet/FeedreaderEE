da3f7ae6f2c416d9a40cd6606d0c8eb9c7cdb606
philippefichet <philippe.fichet@laposte.net>
2014-11-15 15:22:41 +0100

    Formatage du fichier

--- src/main/java/fr/feedreader/buisness/FeedItemBuisness.java
+++ src/main/java/fr/feedreader/buisness/FeedItemBuisness.java
@@ -12,41 +12,19 @@ import javax.persistence.PersistenceContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 @Stateless
 public class FeedItemBuisness {
 
     @PersistenceContext(unitName = "FeedReaderPU")
     private EntityManager em;
-    
+
     private Logger logger = LogManager.getLogger(FeedItemBuisness.class);
-	private Integer itemPerPage = 20;
-	
-	public FeedItem create(FeedItem feedItem) {
+    private Integer itemPerPage = 20;
+
+    public FeedItem create(FeedItem feedItem) {
         try {
             em.persist(feedItem);
-        } catch (Exception e ) {
-            e.printStackTrace();
-            logger.debug("**********************");
-            logger.debug("feedItem.getEnclosure : " + feedItem.getEnclosure());
-            logger.debug("feedItem.getFeedItemId : " + feedItem.getFeedItemId());
-            logger.debug("feedItem.getLink : " + feedItem.getLink());
-            logger.debug("feedItem.getSummary.length : " + feedItem.getSummary().length());
-            logger.debug("feedItem.getSummary : " + feedItem.getSummary());
-            logger.debug("feedItem.getTitle : " + feedItem.getTitle());
-            logger.debug("feedItem.getId : " + feedItem.getId());
-            logger.debug("feedItem.getReaded : " + feedItem.getReaded());
-            logger.debug("feedItem.getUpdated : " + feedItem.getUpdated());
-            logger.debug("feedItem.getFeed : " + feedItem.getFeed());
-            throw e;
-        }
-		return feedItem;
-	}
-	
-	public FeedItem update(FeedItem feedItem) {
-        try {
-            feedItem = em.merge(feedItem);
-        } catch (Exception e ) {
+        } catch (Exception e) {
             e.printStackTrace();
             logger.debug("**********************");
             logger.debug("feedItem.getEnclosure : " + feedItem.getEnclosure());
@@ -62,39 +40,59 @@ public class FeedItemBuisness {
             throw e;
         }
         return feedItem;
-	}
-	
-	public FeedItem setReaded(Integer feedItemId, Boolean readed) {
-		FeedItem feedItem = find(feedItemId);
-		feedItem.setReaded(readed);
-		return update(feedItem);
-	}
-	
-	public FeedItem find(Integer feedItemId) {
-		return em.find(FeedItem.class, feedItemId);
-	}
+    }
 
-	public Long getCount(Integer feedId) {
-		TypedQuery<Long> query = em.createNamedQuery(FeedItem.countByFeedId, Long.class);
-		query.setParameter("feedId", feedId);
+    public FeedItem update(FeedItem feedItem) {
+        try {
+            feedItem = em.merge(feedItem);
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.debug("**********************");
+            logger.debug("feedItem.getEnclosure : " + feedItem.getEnclosure());
+            logger.debug("feedItem.getFeedItemId : " + feedItem.getFeedItemId());
+            logger.debug("feedItem.getLink : " + feedItem.getLink());
+            logger.debug("feedItem.getSummary.length : " + feedItem.getSummary().length());
+            logger.debug("feedItem.getSummary : " + feedItem.getSummary());
+            logger.debug("feedItem.getTitle : " + feedItem.getTitle());
+            logger.debug("feedItem.getId : " + feedItem.getId());
+            logger.debug("feedItem.getReaded : " + feedItem.getReaded());
+            logger.debug("feedItem.getUpdated : " + feedItem.getUpdated());
+            logger.debug("feedItem.getFeed : " + feedItem.getFeed());
+            throw e;
+        }
+        return feedItem;
+    }
+
+    public FeedItem setReaded(Integer feedItemId, Boolean readed) {
+        FeedItem feedItem = find(feedItemId);
+        feedItem.setReaded(readed);
+        return update(feedItem);
+    }
+
+    public FeedItem find(Integer feedItemId) {
+        return em.find(FeedItem.class, feedItemId);
+    }
+
+    public Long getCount(Integer feedId) {
+        TypedQuery<Long> query = em.createNamedQuery(FeedItem.countByFeedId, Long.class);
+        query.setParameter("feedId", feedId);
         return query.getSingleResult();
-	}
-    
-	public Long getTotalPage(Integer feedId) {
-		return (getCount(feedId) / itemPerPage) + 1;
-	}
+    }
 
-	
-	public List<FeedItem> findAll(Integer feedId, Integer page) {
-		TypedQuery<FeedItem> query = em.createNamedQuery(FeedItem.findAllByFeedId, FeedItem.class);
-		query.setParameter("feedId", feedId);
-		query.setMaxResults(itemPerPage);
-		query.setFirstResult((page - 1) * itemPerPage);
-		return query.getResultList();
-	}
+    public Long getTotalPage(Integer feedId) {
+        return (getCount(feedId) / itemPerPage) + 1;
+    }
 
-	public List<FeedItem> findAll(Feed feed) {
-		return feed.getFeedItems();
-	}
+    public List<FeedItem> findAll(Integer feedId, Integer page) {
+        TypedQuery<FeedItem> query = em.createNamedQuery(FeedItem.findAllByFeedId, FeedItem.class);
+        query.setParameter("feedId", feedId);
+        query.setMaxResults(itemPerPage);
+        query.setFirstResult((page - 1) * itemPerPage);
+        return query.getResultList();
+    }
+
+    public List<FeedItem> findAll(Feed feed) {
+        return feed.getFeedItems();
+    }
 
 }
