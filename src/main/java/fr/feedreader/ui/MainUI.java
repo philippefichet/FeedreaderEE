a7d66ebdf5324b04e6f086338ce69cd9a8b682b0
philippefichet <philippe.fichet@laposte.net>
2015-01-10 18:36:06 +0100

    Remise en place des push websocket vaadin

--- src/main/java/fr/feedreader/ui/MainUI.java
+++ src/main/java/fr/feedreader/ui/MainUI.java
@@ -34,7 +34,7 @@ import org.apache.logging.log4j.LogManager;
  */
 @CDIUI(value = "v")
 @Theme(value = "feedreader-dark")
-//@Push
+@Push
 public class MainUI  extends UI {
     
     // Concervation des session pour communication avec plusieur utilisateur (broadcast)
@@ -70,15 +70,14 @@ public class MainUI  extends UI {
             }
         });
         
-//        availableUI.add(this);
-//        
-//        addDetachListener((DetachEvent event) -> {
-//            availableUI.remove(this);
-//        });
-//        
-//        // Autorisation des notifications html
-//        JavaScript.getCurrent().execute("var Notification = window.Notification || window.mozNotification || window.webkitNotification; Notification.requestPermission(function (permission) { console.log('Notification : ' + permission) });");
-//        
+        availableUI.add(this);
+        
+        addDetachListener((DetachEvent event) -> {
+            availableUI.remove(this);
+        });
+        
+        // Autorisation des notifications html
+        JavaScript.getCurrent().execute("var Notification = window.Notification || window.mozNotification || window.webkitNotification; Notification.requestPermission(function (permission) { console.log('Notification : ' + permission) });");
     }
     
     /**
@@ -87,48 +86,48 @@ public class MainUI  extends UI {
      * @param countUnread 
      */
     public void updateFeed(Map<Feed, List<FeedItem>> newFeedItem, Map<Feed, Long> countUnread) {
-//        getUI().access(() -> {
-//            JavaScript javaScript = getPage().getJavaScript();
-//            StringBuilder sb = new StringBuilder();
-//            Long sum = 0L;
-//            Iterator<Feed> i = newFeedItem.keySet().iterator();
-//            while (i.hasNext()) {
-//                Feed feed = i.next();
-//                int count = newFeedItem.get(feed).size();
-//                if (count > 0) {
-//                    sb.append(feed.getName() + " (" + count + "), ");
-//                    sum += count;
-//                }
-//            }
-//            if (sb.length() > 2) {
-//                sb.delete(sb.length()-2, sb.length());
-//            }
-//            
-//            // Si aucun nouvel article fin de notification de mise à jour
-//            if (sum == 0) {
-//                return;
-//            }
-//            
-//            // Notification html
-//            javaScript.execute("new Notification(\"" + sum + " Nouveau(x) article(s)\", {\"body\": \"" + sb.toString() + "\", \"icon\": \"/feedreader/images/feed.png\"});");
-//            
-//            // Mise à jour des compteurs et flux
-//            if (currentView instanceof FeedView) {
-//                FeedView feedView = (FeedView)currentView;
-//                feedView.setCountUnread(countUnread);
-//                feedView.initFeedLayout();
-//            }
-//            
-//            // Mise à jour du compteur et flux
-//            if (currentView instanceof FeedItemView) {
-//                FeedItemView feedItemView = (FeedItemView)currentView;
-//            }
-//            
-//            // Mise à jour du compteur
-//            if (currentView instanceof FeedItemDetailView) {
-//                FeedItemDetailView feedItemDetailView = (FeedItemDetailView)currentView;
-//            }
-//        });
+        getUI().access(() -> {
+            JavaScript javaScript = getPage().getJavaScript();
+            StringBuilder sb = new StringBuilder();
+            Long sum = 0L;
+            Iterator<Feed> i = newFeedItem.keySet().iterator();
+            while (i.hasNext()) {
+                Feed feed = i.next();
+                int count = newFeedItem.get(feed).size();
+                if (count > 0) {
+                    sb.append(feed.getName() + " (" + count + "), ");
+                    sum += count;
+                }
+            }
+            if (sb.length() > 2) {
+                sb.delete(sb.length()-2, sb.length());
+            }
+            
+            // Si aucun nouvel article fin de notification de mise à jour
+            if (sum == 0) {
+                return;
+            }
+            
+            // Notification html
+            javaScript.execute("new Notification(\"" + sum + " Nouveau(x) article(s)\", {\"body\": \"" + sb.toString() + "\", \"icon\": \"/feedreader/images/feed.png\"});");
+            
+            // Mise à jour des compteurs et flux
+            if (currentView instanceof FeedView) {
+                FeedView feedView = (FeedView)currentView;
+                feedView.setCountUnread(countUnread);
+                feedView.initFeedLayout();
+            }
+            
+            // Mise à jour du compteur et flux
+            if (currentView instanceof FeedItemView) {
+                FeedItemView feedItemView = (FeedItemView)currentView;
+            }
+            
+            // Mise à jour du compteur
+            if (currentView instanceof FeedItemDetailView) {
+                FeedItemDetailView feedItemDetailView = (FeedItemDetailView)currentView;
+            }
+        });
     }
     
     /**
@@ -137,18 +136,18 @@ public class MainUI  extends UI {
      * @param countUnread 
      */
     public static void notifyUpdateFeed(Map<Feed, List<FeedItem>> newFeedItem, Map<Feed, Long> countUnread) {
-//        LogManager.getLogger().info("notification de mise à jour de flux vaadin");
-//        System.out.println("newFeedItem = " + newFeedItem);
-//        System.out.println("countUnread = " + countUnread);
-//        for (Iterator<MainUI> it = availableUI.iterator(); it.hasNext();) {
-//            MainUI ui = it.next();
-//            if(ui.isAttached()) {
-//                ui.updateFeed(newFeedItem, countUnread);
-//                LogManager.getLogger().info("Envoyer à \"" + ui + "\"");
-//            } else {
-//                it.remove();
-//                LogManager.getLogger().error("MainUI \"" + ui+ "\" Fermer. Exclusion de la liste des UI active");
-//            }
-//        }
+        LogManager.getLogger().info("notification de mise à jour de flux vaadin");
+        System.out.println("newFeedItem = " + newFeedItem);
+        System.out.println("countUnread = " + countUnread);
+        for (Iterator<MainUI> it = availableUI.iterator(); it.hasNext();) {
+            MainUI ui = it.next();
+            if(ui.isAttached()) {
+                ui.updateFeed(newFeedItem, countUnread);
+                LogManager.getLogger().info("Envoyer à \"" + ui + "\"");
+            } else {
+                it.remove();
+                LogManager.getLogger().error("MainUI \"" + ui+ "\" Fermer. Exclusion de la liste des UI active");
+            }
+        }
     }
 }
